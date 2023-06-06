3db66c4f2e7d4203295450de06b13761eb91d7f9
Serhii-Leonenko <leonenko3149@gmail.com>
2023-06-06 22:59:22 +0300

    bot made async

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import json
 import random
-
-import requests
+import asyncio
+import aiohttp
 
 HOST = "http://127.0.0.1:8000"
 
 
-def read_config_file():
+async def read_config_file():
     with open("var/bot_config.json") as json_file:
         data = json.load(json_file)
         return data
@@ -15,97 +15,109 @@ def read_config_file():
 def generate_random_username_and_password():
     username = "user" + str(random.randint(1, 100000))
     password = "password" + str(random.randint(1, 100000))
-
     return username, password
 
 
-def get_token_for_user(username, password):
-    response = requests.post(
-        f"{HOST}/api/user/login/",
-        json={
-            "username": username,
-            "password": password,
-        },
-    )
-
-    if response.status_code == 200:
-        return response.json().get("access")
-
-
-def signup_users(number_of_users):
-    created_users = []
-
-    while len(created_users) < number_of_users:
-        username, password = generate_random_username_and_password()
-
-        response = requests.post(
-            f"{HOST}/api/user/register/",
+async def get_token_for_user(username, password):
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            f"{HOST}/api/user/login/",
             json={
                 "username": username,
                 "password": password,
             },
         )
 
-        if response.status_code == 201:
+        if response.status == 200:
+            return (await response.json()).get("access")
+
+
+async def signup_user(username, password):
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            f"{HOST}/api/user/register/",
+            json={
+                "username": username,
+                "password": password,
+                "first_name": "test",
+                "last_name": "test",
+            },
+        )
+
+        if response.status == 201:
             print(f"User {username} created successfully")
 
-            token = get_token_for_user(username, password)
+            token = await get_token_for_user(username, password)
 
             if token:
-                created_users.append(
-                    {
-                        "username": username,
-                        "password": password,
-                        "token": token,
-                    }
-                )
+                return {
+                    "username": username,
+                    "password": password,
+                    "token": token,
+                }
+
+
+async def signup_users(number_of_users):
+    tasks = []
+
+    for _ in range(number_of_users):
+        username, password = generate_random_username_and_password()
+        task = asyncio.create_task(signup_user(username, password))
+        tasks.append(task)
+
+    results = await asyncio.gather(*tasks)
+    created_users = [user for user in results if user]
 
     return created_users
 
 
-def create_posts(created_users, max_posts_per_user):
+async def create_posts(created_users, max_posts_per_user):
     created_posts = []
 
     for user in created_users:
         number_of_posts = random.randint(1, max_posts_per_user)
 
-        for i in range(number_of_posts):
-
-            response = requests.post(
-                f"{HOST}/api/posts/",
-                headers={"Authorization": f"Bearer {user['token']}"},
-                json={"title": f"test title {i}", "content": f"test content {i}"},
-            )
-
-            if response.status_code == 201:
-                print(f"Post created successfully for user {user['username']}")
-                created_posts.append(
-                    {"username": user["username"], "post_id": response.json()["id"]}
+        async with aiohttp.ClientSession() as session:
+            for i in range(number_of_posts):
+                response = await session.post(
+                    f"{HOST}/api/posts/",
+                    headers={"Authorization": f"Bearer {user['token']}"},
+                    json={"title": f"test title {i}", "content": f"test content {i}"},
                 )
+
+                if response.status == 201:
+                    print(f"Post created successfully for user {user['username']}")
+                    created_posts.append(
+                        {
+                            "username": user["username"],
+                            "post_id": (await response.json())["id"],
+                        }
+                    )
 
     return created_posts
 
 
-def like_posts(created_users, created_posts, max_likes_per_user):
+async def like_posts(created_users, created_posts, max_likes_per_user):
     for user in created_users:
         number_of_likes = random.randint(1, max_likes_per_user)
 
-        for i in range(number_of_likes):
-            post = random.choice(created_posts)
+        async with aiohttp.ClientSession() as session:
+            for _ in range(number_of_likes):
+                post = random.choice(created_posts)
 
-            response = requests.post(
-                f"{HOST}/api/posts/{post['post_id']}/like-unlike/",
-                headers={"Authorization": f"Bearer {user['token']}"},
-            )
-
-            if response.status_code == 200:
-                print(
-                    f"Post {post['post_id']} liked successfully by user {user['username']}"
+                response = await session.post(
+                    f"{HOST}/api/posts/{post['post_id']}/like-unlike/",
+                    headers={"Authorization": f"Bearer {user['token']}"},
                 )
 
+                if response.status == 200:
+                    print(
+                        f"Post {post['post_id']} liked successfully by user {user['username']}"
+                    )
 
-def main():
-    config = read_config_file()
+
+async def main():
+    config = await read_config_file()
     print(config)
     number_of_users = config["number_of_users"]
     max_posts_per_user = config["max_posts_per_user"]
@@ -115,10 +127,10 @@ def main():
         print("Number of users is not defined in config file")
         return
 
-    created_users = signup_users(config["number_of_users"])
-    created_posts = create_posts(created_users, max_posts_per_user)
-    like_posts(created_users, created_posts, max_likes_per_user)
+    created_users = await signup_users(config["number_of_users"])
+    created_posts = await create_posts(created_users, max_posts_per_user)
+    await like_posts(created_users, created_posts, max_likes_per_user)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
